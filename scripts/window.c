3393c13e3f039bec49cb76f85ac78ad5d68d04cd
ljiang09 <ljiang@olin.edu>
2023-02-24 18:27:35 -0500

    add background image, resize

--- scripts/window.c
+++ scripts/window.c
@@ -1,16 +1,17 @@
 #include <stdio.h> /* printf and fprintf */
 #include <stdbool.h>
 #include <stdlib.h>
-
 #include <SDL2/SDL_image.h>
 
 //sudo apt-get install libsdl2-dev
-// use compiler flags -lSDL2 and -lSDL2_image  // gcc -o window scripts/window.c -lSDL2 -lSDL2_image
+// use compiler flags -lSDL2 and -lSDL2_image
+// gcc -o window scripts/window.c -lSDL2 -lSDL2_image
 //sudo apt-get install libsdl2-image
 
 /* Sets constants */
-#define WIDTH 800
-#define HEIGHT 600
+// replace the width and height with screen size
+#define WIDTH 1440
+#define HEIGHT 900
 #define DELAY 3000
 
 int main (int argc, char **argv)
@@ -39,12 +40,12 @@ int main (int argc, char **argv)
 
   /* Creates a SDL window */
   window = SDL_CreateWindow("SDL Example", /* Title of the SDL window */
-			    SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
-			    SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
-			    WIDTH, /* Width of the window in pixels */
-			    HEIGHT, /* Height of the window in pixels */
-			    SDL_WINDOW_BORDERLESS ||
-          SDL_WINDOW_MAXIMIZED); /* Additional flag(s) */
+                            SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                            SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                            WIDTH, /* Width of the window in pixels */
+                            HEIGHT, /* Height of the window in pixels */
+                            SDL_WINDOW_BORDERLESS ||
+                            SDL_WINDOW_MAXIMIZED); /* Additional flag(s) */
   
   /* Checks if window has been created; if not, exits program */
   if (window == NULL) {
@@ -54,14 +55,13 @@ int main (int argc, char **argv)
 
   SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (renderer == NULL) {
-	    printf("renderer error\n");
-	return 4;
-} 
-  //replace with lines that detect max resolution
-  int res_width = 1920;
-  int res_height = 1080;
+    printf("renderer error\n");
+    return 4;
+  }
 
-  if (SDL_RenderSetLogicalSize(renderer, res_width, res_height) != 0){
+  //replace with lines that detect max resolution
+  
+  if (SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT) != 0){
     printf("resolution setting error\n");
 
     printf("%s\n",SDL_GetError());
@@ -69,26 +69,24 @@ int main (int argc, char **argv)
     return 4;
   }
 
-
-  SDL_Surface * image = IMG_Load("assets/boat.png");
+  SDL_Surface * image = IMG_Load("background.png");
   if (image == NULL) {
 	  printf("error loading image\n");
-	return 5;
-}
+    return 5;
+  }
 
 
   SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
 
   if (texture == NULL) {
-	  printf("error creating texture\n");
-	return 6;
-}
+    printf("error creating texture\n");
+    return 6;
+  }
 
 
 
   //dont remember why we need this but apparently it prevents memory leaks
   //SDL_FreeSurface(image);
-  /* Pauses all SDL subsystems for a variable amount of milliseconds */
 
   fprintf(stdout,"window initialized\n");
   while (!quit)
@@ -99,16 +97,15 @@ int main (int argc, char **argv)
       {    
         //if you press a key
         case SDL_KEYDOWN:
-            switch(event.key.keysym.sym){
+          switch(event.key.keysym.sym) {
+            case SDLK_ESCAPE:
+            printf("got here\n");
+            quit = true;
+            break;
+          }
 
-              case SDLK_ESCAPE:
-              printf("got here");
-              quit = true;
-              break;
-            }
-
-            //break out of larger SDL_KEYDOWN
-            break;  
+          //break out of larger SDL_KEYDOWN
+          break;  
               
         case SDLK_ESCAPE:
           quit = true;
