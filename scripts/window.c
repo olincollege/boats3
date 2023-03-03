2aef4998ac49e4bfa3bfe822fff459e505d4a5a7
Luke Witten <lhwitten@gmail.com>
2023-03-02 23:26:30 -0500

    made event handle function

--- scripts/window.c
+++ scripts/window.c
@@ -14,6 +14,9 @@
 #define HEIGHT 1080
 #define DELAY 3000
 
+//this is a global
+
+bool quit = false;
 
 
 void end_program(SDL_Texture *texture, SDL_Surface *image, SDL_Renderer *renderer, SDL_Window *window) {
@@ -30,13 +33,44 @@ void end_program(SDL_Texture *texture, SDL_Surface *image, SDL_Renderer *rendere
 }
 
 
+void Handle_Event(SDL_Event event){
 
+    switch (event.type)
+      {    
+        //if you press a key
+        case SDL_KEYDOWN:
+            switch(event.key.keysym.sym){
+
+              case SDLK_ESCAPE:
+              quit = true;
+              break;
+            }
+    switch (event.type)
+    {    
+      // if you press a key
+      case SDL_KEYDOWN:
+        switch(event.key.keysym.sym) {
+          case SDLK_ESCAPE:
+          printf("got here\n");
+          quit = true;
+          break;
+        }
+
+        //break out of larger SDL_KEYDOWN
+        break;  
+
+      case SDL_QUIT:
+        quit = true;
+        break;
+    }
+}
+
+}
 
 int main(int argc, char **argv) {
   /* Initialises data */
   SDL_Window *window = NULL;
 
-  bool quit = false;
   SDL_Event event;
 
   /*
@@ -73,12 +107,12 @@ int main(int argc, char **argv) {
     printf("renderer error\n");
     return 4;
   }
-
+  /*
   if (SDL_RenderSetLogicalSize(renderer, WIDTH, HEIGHT) != 0){
     printf("resolution setting error\n");
     printf("%s\n", SDL_GetError());
     return 5;
-  }
+  }//*/
 
   SDL_Surface * image = IMG_Load("assets/background.png");
   if (image == NULL) {
@@ -92,49 +126,65 @@ int main(int argc, char **argv) {
     return 7;
   }
 
+  SDL_Surface * boat_image = IMG_Load("assets/boat.png");
+  if (image == NULL) {
+	  printf("error loading image\n");
+    return 6;
+  }
+
+  SDL_Texture * boat_texture = SDL_CreateTextureFromSurface(renderer, boat_image);
+  if (texture == NULL) {
+    printf("error creating texture\n");
+    return 7;
+  }
+
+  SDL_Rect dstrect;
+  dstrect.x = 20;
+  dstrect.y = 20;
+  dstrect.w = 350;
+  dstrect.h = 350;
+
+  SDL_Rect dstrect2;
+  dstrect2.x = 500;
+  dstrect2.y = 0;
+  dstrect2.w = 550;
+  dstrect2.h = 350;
+
+  int cycle = 0;
 
   fprintf(stdout, "window initialized\n");
   while (!quit) {
-    SDL_WaitEvent(&event);
 
-      switch (event.type)
-      {    
-        //if you press a key
-        case SDL_KEYDOWN:
-            switch(event.key.keysym.sym){
+    cycle++;
+    cycle = cycle%2;
 
-              case SDLK_ESCAPE:
-              quit = true;
-              break;
-            }
-    switch (event.type)
-    {    
-      // if you press a key
-      case SDL_KEYDOWN:
-        switch(event.key.keysym.sym) {
-          case SDLK_ESCAPE:
-          printf("got here\n");
-          quit = true;
-          break;
-        }
-
-        //break out of larger SDL_KEYDOWN
-        break;  
-            
-      case SDLK_ESCAPE:
-        quit = true;
-        break;
-
-      case SDL_QUIT:
-        quit = true;
-        break;
+    if (SDL_PollEvent(&event)){
+      Handle_Event(event);
     }
+
+
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+    printf("%i\n",cycle);
+    if (cycle){
+      SDL_RenderCopy(renderer, boat_texture, NULL, &dstrect);
+    }
+    else{
+      SDL_RenderCopy(renderer, texture, NULL, &dstrect2);
+    }
     SDL_RenderPresent(renderer);
   }
+  
+
+  SDL_DestroyTexture(boat_texture);
+  printf("got here 4\n");
+  SDL_FreeSurface(boat_image);
+  printf("got here 5\n");
 
   end_program(texture, image, renderer, window);
+
+  
   
   return 0;
 }
