a92f32e4d30732d19161b2aec9f941d2c304d397
Aditi Vinod <avinod@olin.edu>
2023-03-03 04:59:45 -0500

    #8 - Sprite Struct Test.

--- scripts/window.c
+++ scripts/window.c
@@ -1,56 +1,62 @@
-#include <stdio.h> /* printf and fprintf */
-#include <stdbool.h>
-#include <stdlib.h>
 #include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include <stdio.h> /* printf and fprintf */
+#include <stdlib.h>
 
-//sudo apt-get install libsdl2-dev
-// use compiler flags -lSDL2 and -lSDL2_image
-// gcc -o window scripts/window.c -lSDL2 -lSDL2_image
-//sudo apt-get install libsdl2-image
+// sudo apt-get install libsdl2-dev
+//  use compiler flags -lSDL2 and -lSDL2_image
+//  gcc -o window scripts/window.c -lSDL2 -lSDL2_image
+// sudo apt-get install libsdl2-image
 
 /* Sets constants */
 // replace the width and height with screen size
-#define WIDTH 1920  // this is based on the background image size currently
+#define WIDTH 1920 // this is based on the background image size currently
 #define HEIGHT 1080
 #define DELAY 3000
 
-//this is a global
+// this is a global
 
 bool quit = false;
 
-typedef struct Animation{
-  SDL_Texture * texture;
+typedef struct sprite {
+  SDL_Texture *texture;
+  SDL_Rect *rect;
+  const char *path;
+} sprite;
+
+typedef struct Animation {
+  SDL_Texture *texture;
   int width;
   int height;
   int ypos;
   int num_frames;
-  int frames_loop[30]; 
+  int frames_loop[30];
   int frame_index;
-}Animation;
+} Animation;
 
-//Animation kittykat = {
+// Animation kittykat = {
 
-void loop_Animation(Animation *loop, SDL_Renderer* renderer,SDL_Rect *box_ptr){
+void loop_Animation(Animation *loop, SDL_Renderer *renderer,
+                    SDL_Rect *box_ptr) {
 
   int xpos;
-  
-  xpos = loop->frames_loop[loop->frame_index]*loop->width;
-  printf("%d\n",xpos);
-  
 
-  
-  SDL_Rect crop_sprite = {.x=xpos,.y=loop->ypos,.w=loop->width,.h=loop->height};
+  xpos = loop->frames_loop[loop->frame_index] * loop->width;
+  // printf("%d\n", xpos);
 
-  
-  if (0!= SDL_RenderCopy(renderer, loop->texture, &crop_sprite, box_ptr)){
+  SDL_Rect crop_sprite = {
+      .x = xpos, .y = loop->ypos, .w = loop->width, .h = loop->height};
+
+  if (0 != SDL_RenderCopy(renderer, loop->texture, &crop_sprite, box_ptr)) {
     printf("error animating");
   }
-  //update the position in the loop
-  loop->frame_index = loop->frame_index +1;
+  // update the position in the loop
+  loop->frame_index = loop->frame_index + 1;
   loop->frame_index = loop->frame_index % loop->num_frames;
 }
 
-void end_program(SDL_Texture *texture, SDL_Surface *image, SDL_Renderer *renderer, SDL_Window *window) {
+void end_program(SDL_Texture *texture, SDL_Surface *image,
+                 SDL_Renderer *renderer, SDL_Window *window) {
   SDL_DestroyTexture(texture);
   printf("got here 4\n");
   SDL_FreeSurface(image);
@@ -63,39 +69,35 @@ void end_program(SDL_Texture *texture, SDL_Surface *image, SDL_Renderer *rendere
   printf("got here 8\n");
 }
 
+void Handle_Event(SDL_Event event) {
 
-void Handle_Event(SDL_Event event){
+  switch (event.type) {
+  // if you press a key
+  case SDL_KEYDOWN:
+    switch (event.key.keysym.sym) {
 
-    switch (event.type)
-      {    
-        //if you press a key
-        case SDL_KEYDOWN:
-            switch(event.key.keysym.sym){
-
-              case SDLK_ESCAPE:
-              quit = true;
-              break;
-            }
-    switch (event.type)
-    {    
-      // if you press a key
-      case SDL_KEYDOWN:
-        switch(event.key.keysym.sym) {
-          case SDLK_ESCAPE:
-          printf("got here\n");
-          quit = true;
-          break;
-        }
-
-        //break out of larger SDL_KEYDOWN
-        break;  
-
-      case SDL_QUIT:
+    case SDLK_ESCAPE:
+      quit = true;
+      break;
+    }
+    switch (event.type) {
+    // if you press a key
+    case SDL_KEYDOWN:
+      switch (event.key.keysym.sym) {
+      case SDLK_ESCAPE:
+        printf("got here\n");
         quit = true;
         break;
-    }
-}
+      }
 
+      // break out of larger SDL_KEYDOWN
+      break;
+
+    case SDL_QUIT:
+      quit = true;
+      break;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
@@ -105,35 +107,37 @@ int main(int argc, char **argv) {
   SDL_Event event;
 
   /*
-  * Initialises the SDL video subsystem (as well as the events subsystem).
-  * Returns 0 on success or a negative error code on failure using SDL_GetError().
-  */
+   * Initialises the SDL video subsystem (as well as the events subsystem).
+   * Returns 0 on success or a negative error code on failure using
+   * SDL_GetError().
+   */
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
     return 1;
   }
 
   if (IMG_Init(IMG_INIT_PNG) == 0) {
-	  printf("Error SDL2_image Initialization\n");
+    printf("Error SDL2_image Initialization\n");
     return 2;
   }
 
   // Creates a SDL window
-  window = SDL_CreateWindow("SDL Example", /* Title of the SDL window */
-                            SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
-                            SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
-                            WIDTH, /* Width of the window in pixels */
-                            HEIGHT, /* Height of the window in pixels */
-                            SDL_WINDOW_BORDERLESS ||
-                            SDL_WINDOW_MAXIMIZED); /* Additional flag(s) */
-  
+  window = SDL_CreateWindow(
+      "SDL Example",           /* Title of the SDL window */
+      SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+      SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+      WIDTH,                   /* Width of the window in pixels */
+      HEIGHT,                  /* Height of the window in pixels */
+      SDL_WINDOW_BORDERLESS || SDL_WINDOW_MAXIMIZED); /* Additional flag(s) */
+
   // Checks if window has been created; if not, exits program
   if (window == NULL) {
     fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
     return 3;
   }
 
-  SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (renderer == NULL) {
     printf("renderer error\n");
     return 4;
@@ -145,41 +149,56 @@ int main(int argc, char **argv) {
     return 5;
   }//*/
 
-  SDL_Surface * image = IMG_Load("assets/background.png");
+  SDL_Surface *image = IMG_Load("assets/background.png");
   if (image == NULL) {
-	  printf("error loading image\n");
+    printf("error loading image\n");
     return 6;
   }
 
-  SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
   if (texture == NULL) {
     printf("error creating texture\n");
     return 7;
   }
 
-  SDL_Surface * boat_image = IMG_Load("assets/boat.png");
+  SDL_Surface *boat_image = IMG_Load("assets/boat.png");
   if (image == NULL) {
-	  printf("error loading image\n");
+    printf("error loading image\n");
     return 6;
   }
 
-  SDL_Texture * boat_texture = SDL_CreateTextureFromSurface(renderer, boat_image);
+  SDL_Texture *boat_texture =
+      SDL_CreateTextureFromSurface(renderer, boat_image);
   if (texture == NULL) {
     printf("error creating texture\n");
     return 7;
   }
 
-  SDL_Surface * cat_image = IMG_Load("assets/Catsprites.png");
+  SDL_Surface *cat_image = IMG_Load("assets/Catsprites.png");
   if (image == NULL) {
-	  printf("error loading image\n");
+    printf("error loading image\n");
     return 6;
   }
 
-  SDL_Texture * cat_texture = SDL_CreateTextureFromSurface(renderer, cat_image);
+  SDL_Texture *cat_texture = SDL_CreateTextureFromSurface(renderer, cat_image);
   if (texture == NULL) {
     printf("error creating texture\n");
     return 7;
   }
+
+  /*const char *hpath = "assets/boat.png";
+  SDL_Surface *husky_image = IMG_Load(hpath);
+  SDL_Texture *husky_texture =
+      SDL_CreateTextureFromSurface(renderer, husky_image);
+  SDL_Rect husky_rect = {500, 0, 350, 350};*/
+
+  // sprite husky = {husky_texture, &husky_rect, hpath};
+  SDL_Rect husky_rect = {0, 500, 350, 350};
+  sprite husky = {
+      SDL_CreateTextureFromSurface(renderer, IMG_Load("assets/boat.png")),
+      &husky_rect, "assets/boat.png"};
+
+  // sprite husky = {husky_texture, &(sprite){500, 0, 350, 350}, hpath};
 
   SDL_Rect dstrect;
   dstrect.x = 500;
@@ -205,58 +224,60 @@ int main(int argc, char **argv) {
   srcrect2.w = 100;
   srcrect2.h = 100;
 
+  // kittykat spritesheet
+  /*
+    typedef struct Animation{
+    SDL_Texture * texture;
+    int width;
+    int height;
+    int ypos;
+    int num_frames;
+    int frames_loop[30];
+    int frame_index;
+  }Animation;//*/
 
-  //kittykat spritesheet
-/*
-  typedef struct Animation{
-  SDL_Texture * texture;
-  int width;
-  int height;
-  int ypos;
-  int num_frames;
-  int frames_loop[30]; 
-  int frame_index;
-}Animation;//*/
-
-  Animation kittykat_walk = {.texture=boat_texture,.width=200,.height=200,.ypos=200,.num_frames=7,.frames_loop = {0,1,2,2,2,2,1},.frame_index=0};
+  Animation kittykat_walk = {.texture = boat_texture,
+                             .width = 200,
+                             .height = 200,
+                             .ypos = 200,
+                             .num_frames = 7,
+                             .frames_loop = {0, 1, 2, 2, 2, 2, 1},
+                             .frame_index = 0};
 
   int cycle = 0;
 
   fprintf(stdout, "window initialized\n");
   while (!quit) {
-
     cycle++;
-    cycle = cycle%2;
+    cycle = cycle % 2;
 
-    if (SDL_PollEvent(&event)){
+    if (SDL_PollEvent(&event)) {
       Handle_Event(event);
     }
 
-
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderCopy(renderer, husky.texture, NULL, husky.rect);
 
-    //printf("%i\n",cycle);
-    if (cycle){
+    // printf("%i\n",cycle);
+    if (cycle) {
       SDL_RenderCopy(renderer, boat_texture, NULL, &dstrect);
-    }
-    else{
-      //test of the crop image feature
+
+    } else {
+      // test of the crop image feature
       SDL_RenderCopy(renderer, texture, &srcrect2, &dstrect2);
     }
-    loop_Animation(&kittykat_walk,renderer,&dstrect3);
+    loop_Animation(&kittykat_walk, renderer, &dstrect3);
     SDL_RenderPresent(renderer);
   }
-  
 
   SDL_DestroyTexture(boat_texture);
   printf("got here 4\n");
   SDL_FreeSurface(boat_image);
   printf("got here 5\n");
+  // SDL_DestroyTexture(husky.texture);
 
   end_program(texture, image, renderer, window);
 
-  
-  
   return 0;
 }
