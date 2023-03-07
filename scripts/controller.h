e0a14e10bfbba5cc14fca6479bfd993bdc5659d4
richardli03 <rli@olin.edu>
2023-03-07 15:46:48 -0500

    added documentation for event handler

--- scripts/controller.h
+++ scripts/controller.h
@@ -1,5 +1,5 @@
 /*
-oh no
+The controller, responsible for handling inputs from the user.
 */
 
 #pragma once
@@ -9,7 +9,13 @@ oh no
 #include <stdlib.h>
 
 extern bool quit = false;
-/*
+/**
 Handle an event
-*/
+
+Given an SDL event queue, identify what event is occuring
+(generally, what key is being pressed) and produce
+the correct corresponding behavior
+
+* @param event An SDL event queue to manage
+**/
 void Handle_Event(SDL_Event event);
